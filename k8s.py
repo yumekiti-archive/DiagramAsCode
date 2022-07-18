a0b8b701ca13463f300130c47f7ddd408cbf22d2
yumekiti <yumekiti120440@gmail.com>
2022-07-18 23:32:37 +0900

    `chenge` k8s

--- k8s.py
+++ k8s.py
@@ -1,7 +1,25 @@
-# diagram.py
-from diagrams import Diagram
+from diagrams import Cluster, Diagram, Node, Edge
 from diagrams.k8s.compute import Pod
-from diagrams.k8s.infra import Node
+from diagrams.k8s.network import Ing
+from diagrams.gcp.network import LoadBalancing
+from diagrams.onprem.network import Nginx
 
-with Diagram("Web Service", show=False):
-  Node() >> Pod("web")
+with Diagram("Cluster nesting", show=False):
+
+    gcp_lb = LoadBalancing("GCP LB")
+
+    with Cluster("Kubernetes"):
+
+        with Cluster("Nginx"):
+            nginx= Nginx("")
+
+        with Cluster("MyApp"):
+            myapp_ing = Ing("")
+            with Cluster("Pods"):
+                myapp_pods = Pod("myapp")
+
+        with Cluster("MySQL"):
+            myapp_db = Pod("myapp-db")
+
+    gcp_lb >> Edge(headport="c", tailport="c", minlen="1", lhead='cluster_Kubernetes') >> nginx
+    nginx >> Edge(headport="c", tailport="c", minlen="1", lhead='cluster_MyApp') >> myapp_ing >> Edge(headport="c", tailport="c", minlen="1", lhead='cluster_MyApp pods') >> myapp_pods >> myapp_db
